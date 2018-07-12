61d8225a13932e1c32d99476060304aa059c4cf3
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-07-12 09:37:45 -0500

    simplifica plantillas

---
+++ config/routes.rb
@@ -0,0 +1,34 @@
+Rails.application.routes.draw do
+
+  scope 'somosdefensores/sivel2' do 
+    devise_scope :usuario do
+      get 'sign_out' => 'devise/sessions#destroy'
+
+      # El siguiente para superar mala generación del action en el formulario
+      # cuando se monta en sitio diferente a / y se autentica mal (genera 
+      # /puntomontaje/puntomontaje/usuarios/sign_in )
+      if (Rails.configuration.relative_url_root != '/') 
+        ruta = File.join(Rails.configuration.relative_url_root, 
+                         'usuarios/sign_in')
+        post ruta, to: 'devise/sessions#create'
+        get  ruta, to: 'devise/sessions#new'
+      end
+    end
+
+    devise_for :usuarios, :skip => [:registrations], module: :devise
+    as :usuario do
+      get 'usuarios/edit' => 'devise/registrations#edit', 
+        :as => 'editar_registro_usuario'    
+      put 'usuarios/:id' => 'devise/registrations#update', 
+        :as => 'registro_usuario'            
+    end
+    resources :usuarios, path_names: { new: 'nuevo', edit: 'edita' } 
+
+    root 'sivel2_gen/hogar#index'
+  end
+
+  mount Sip::Engine => "/somosdefensores/sivel2", as: 'sip'
+  mount Sivel2Gen::Engine => "/somosdefensores/sivel2", as: 'sivel2_gen'
+  mount Heb412Gen::Engine => "/somosdefensores/sivel2", as: 'heb412_gen'
+
+end
