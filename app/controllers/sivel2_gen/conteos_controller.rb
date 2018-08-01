acdf660cd6011c1f08c1c0ade6e9e0b379431540
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-08-01 06:02:37 -0500

    Mejora conteos

---
+++ app/controllers/sivel2_gen/conteos_controller.rb
@@ -0,0 +1,24 @@
+# encoding: UTF-8
+
+require 'sivel2_gen/concerns/controllers/conteos_controller'
+
+module Sivel2Gen
+  class ConteosController < ApplicationController
+
+    include Sivel2Gen::Concerns::Controllers::ConteosController
+
+    # Llena variables de clase: @opsegun, @titulo_personas,
+    # @titulo_personas_fecha y otras nuevas relacionads con filtros
+    # (prefijo p)
+    def personas_filtros_especializados
+      @opsegun =  ["", "ETNIA", #"FILIACIÓN", 
+                   "MES CASO", "ORGANIZACIÓN", "PROFESIÓN", 
+                   "RANGO DE EDAD", "SECTOR SOCIAL", "SEXO", 
+                   Sivel2Gen::Victima.human_attribute_name(:vinculoestado).upcase
+      ]
+      @titulo_personas = 'Demografía de Víctimas'
+      @titulo_personas_fecha = 'Fecha del Caso'
+    end
+
+  end
+end
