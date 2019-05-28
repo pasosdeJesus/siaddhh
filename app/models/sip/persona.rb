45b02f6f6df93ab41ab840f3459593a6ed217458
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-05-28 11:02:16 -0500

    Actualiza a sip que tiene opcional relacion entre actrosocial_persona y actorsocial y perfil

--- app/models/sip/persona.rb
+++ app/models/sip/persona.rb
@@ -8,7 +8,8 @@ module Sip
 
     has_one :actorsocial_persona, foreign_key: "persona_id", validate: true, 
       class_name: 'Sip::ActorsocialPersona'
-    accepts_nested_attributes_for :actorsocial_persona, :reject_if => :all_blank
+    accepts_nested_attributes_for :actorsocial_persona, 
+      :reject_if => :all_blank
 
   end
 end
