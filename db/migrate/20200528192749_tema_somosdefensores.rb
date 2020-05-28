6913aed81df1124747a38c48b413df7cb408e78d
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-05-28 15:42:07 -0500

    Afina tema

--- db/migrate/20200528192749_tema_somosdefensores.rb
+++ db/migrate/20200528192749_tema_somosdefensores.rb
@@ -3,7 +3,7 @@ class TemaSomosdefensores < ActiveRecord::Migration[6.0]
     execute <<-SQL
     UPDATE public.sip_tema SET 
       nombre='INSTITUCIONAL SOMOS DEFENSORES',
-      colorom_fondo = '#f2d9d0',
+      fondo = '#f2d9d0',
       color_fuente = '#000000',
       nav_ini = '#f2a285',
       nav_fin = '#f24405',
@@ -21,10 +21,23 @@ class TemaSomosdefensores < ActiveRecord::Migration[6.0]
       alerta_exito_fondo = '#f2a285',
       alerta_exito_fuente = '#000000',
       alerta_problema_fondo = '#bf1b28',
-      alerta_problema_fuente = '#ffffff',
+      alerta_problema_fuente = '#ffffff'
       WHERE id=1;
-    SQL
 
+    INSERT INTO public.sip_tema (id, nombre, observaciones, nav_ini, 
+      nav_fin, nav_fuente, fondo_lista, fechacreacion, fechadeshabilitacion, 
+      created_at, updated_at, btn_primario_fondo_ini, btn_primario_fondo_fin, 
+      btn_primario_fuente, btn_peligro_fondo_ini, btn_peligro_fondo_fin, 
+      btn_peligro_fuente, btn_accion_fondo_ini, btn_accion_fondo_fin, 
+      btn_accion_fuente, alerta_exito_fondo, alerta_exito_fuente, 
+      alerta_problema_fondo, alerta_problema_fuente, fondo, color_fuente, 
+      color_flota_subitem_fuente, color_flota_subitem_fondo) VALUES (2, 
+      'AZUL POR OMISIÓN', NULL, '#266dd3', '#266dd3', '#ffffff', '#95c4ff', 
+      '2019-07-15', NULL, '2019-07-15 00:00:00', '2019-12-06 11:28:40.417106', 
+      '#0088cc', '#0044cc', '#ffffff', '#ee5f5b', '#bd362f', '#ffffff', 
+      '#ffffff', '#e6e6e6', '#000000', '#dff0d8', '#468847', '#f8d7da', 
+      '#721c24', '#ffffff', '#000000', NULL, NULL);
+    SQL
   end
 
   def down
