eac4cd46b0f7f3e04d24d005adb320bfa6cd80b7
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-06-25 15:34:10 -0500

    Cambia forma de detectar cuando iniciar R y cambia grafico de victimazaciones

--- app/controllers/fil23_gen/graficar_r_controller.rb
+++ app/controllers/fil23_gen/graficar_r_controller.rb
@@ -3,6 +3,163 @@
 module Fil23Gen
   class GraficarRController < ApplicationController
 
+    # Si hace falta arranca aplicación shiny
+    def arranca_ap_shiny
+      # Como una aplicación shiny puede responder a diversos usuarios
+      # lo que hacemos por ahora es lanzar una que atienda a todos sobre
+      # un proceso nuevo que no bloquee la aplicación rails.
+      # Solo vuelve a iniciarse si un usuario solicita la gráfica
+      # y se detecta que no está operando
+      # Esto no resuelve autenticación, pues cualquie usuario podría
+      # verla corriendo si adivina el puerto.  Pero es un avance, para
+      # autenticación tal vez podría implementarse entre otros con
+      # datos de sesión que parece shiny puede usar.
+
+      ult_numproc = -1
+      pact = [] # Proceso ruby donde se corrió R y que sigue activo
+      lproc = Sip::ProcesosHelper.procesos_OpenBSD
+      # En modo desarrollo, usando puma la estructura de procesos cuando no se ha ejecutado shiny es:
+      # - bin/corre.sh
+      #   |- ruby: puma 4.3.5 (ssl://...) [sivel2_somosdefensores] (ruby27)
+      #      |- /usr/local/lib/R/bin/exec/R --interactive --slave
+      #
+      # Y una vez se ejecuta shiny en un proceso hijo (eliminando la instancia inicial de R e iniciando una en el proceso hijo):
+      # - bin/corre.sh
+      #   |- ruby: puma 4.3.5 (ssl://...) [sivel2_somosdefensores] (ruby27)
+      #     |- ruby: puma 4.3.5 (ssl://...) [sivel2_somosdefensores] (ruby27)
+      #        |- /usr/local/lib/R/bin/exec/R --interactive --slave
+      # - sh -c /usr/local/bin/xdg-open "http://1...:2902" > /dev/null 2>&1 || /usr/local/bin/xdg-open "http://192.168.77.78:2902" &
+      #   |- /bin/sh /usr/local/bin/xdg-open http://1...:2902"
+      #     |- chrome: --disable-features=WebAssembly,AsmJsToWebAssembly,WebAssemblyStreaming --js-flags=--noexpose-wasm http://1...:2902 (chrome)
+      #     |- /bin/sh /usr/local/bin/xdg-settings check default-web-browser chromium-browser.desktop
+      #     |- /bin/sh /usr/local/bin/xdg-settings check default-web-browser chromium-browser.desktop
+      #     |- /bin/sh /usr/local/bin/xdg-mime query default x-scheme-handler/http
+      #     |- xprop -root
+      #     |- grep -i ^xfce_desktop_window
+      #
+      # Y una vez se cierra el chrome que intenta abrir queda:
+      #  - /bin/sh bin/corre.sh
+      #    |- ruby: puma 4.3.5 (ssl://1...) [sivel2_somosdefensores] (ruby27)
+      #       |- ruby: puma 4.3.5 (ssl://1...) [sivel2_somosdefensores] (ruby27)
+      #         |- /usr/local/lib/R/bin/exec/R --interactive --slave
+      #
+      # Y en modo producción con Unicorn cuando no ha ejecutado shiny:
+      #
+      #  - ruby27: unicorn_rails master -c /var/www/htdocs/sivel2_somosdefensores/config/unicorn.conf.minimal.rb -E production -D (ruby27)
+      #   |- ruby27: unicorn_rails worker[1] -c /var/www/htdocs/sivel2_somosdefensores/config/unicorn.conf.minimal.rb -E production -D (ruby27)
+      #     |- /usr/local/lib/R/bin/exec/R --interactive --slave
+      #   |- ruby27: unicorn_rails worker[0] -c /var/www/htdocs/sivel2_somosdefensores/config/unicorn.conf.minimal.rb -E production -D (ruby27)
+      #     |- /usr/local/lib/R/bin/exec/R --interactive --slave
+      #
+      #  Cuando ejecuta shiny:
+      #
+      #  - ruby27: unicorn_rails master -c /var/www/htdocs/sivel2_somosdefensores/config/unicorn.conf.minimal.rb -E production -D (ruby27)
+      #   |- ruby27: unicorn_rails worker[1] -c /var/www/htdocs/sivel2_somosdefensores/config/unicorn.conf.minimal.rb -E production -D (ruby27)
+      #     |- ruby27: unicorn_rails worker[1] -c /var/www/htdocs/sivel2_somosdefensores/config/unicorn.conf.minimal.rb -E production -D (ruby27)
+      #       |- /usr/local/lib/R/bin/exec/R --interactive --slave
+      #   |- ruby27: unicorn_rails worker[0] -c /var/www/htdocs/sivel2_somosdefensores/config/unicorn.conf.minimal.rb -E production -D (ruby27)
+      #     |- /usr/local/lib/R/bin/exec/R --interactive --slave
+      
+      # En todos los casos los proceso comparten el mismo gpid
+      #
+      # El caso de unicorn muestra que la forma en la que estabamos
+      # manejandolo no prospera, porque replica creación de proceso 
+      # R por cada trabajador de unicorn.  
+      # En realidad basta uno, porque todos los trabajadores pueden
+      # dirigir al mismo puerto.
+      #
+      # Hemos notado que en ocasiones que el proceso R pero no el
+      # trbajador, y en ocasiones el trabajador pero no el maestro.
+      # Incluso arrancando de nuevo el maestro.
+      # En ocasiones responde por el puerto pero con un mensjae de error.
+      #
+      # Nos parece mejor detectar si el puerto reservador ya está bajo el 
+      # control de un proceso con el gpid del trabajador que corre y en caso
+      # afirmativo verificar que responda shiny.  Si no se cumple alguna
+      # de estas condiciones, mejor matar el proceso que corre en 
+      # el puerto reservado y volver a iniciar R y shiny.
+      
+      ini = true # Inicializar R con shiny  
+      miproc = Process.pid 
+      puts "miproc=#{miproc}"
+      dmiproc = lproc.select{|p| p[:pid].to_i == miproc}[0]
+      puts "dmiproc=#{dmiproc}"
+      fspuerto = `fstat | grep :#{@fil23_gen_op[:puerto]}$`.split(" ")
+      puts "fspuerto=#{fspuerto}"
+      if fspuerto.length != 0
+        # Verificamos si el proceso que maneja puerto reservado para shiny
+        # está en el mismo grupo de este proceso
+        ppuerto = fspuerto[2].to_i
+        puts "ppuerto=#{ppuerto}"
+        puts "lproc=#{lproc}"
+        byebug
+        dppuerto = lproc.select{|p| p[:pid].to_i == ppuerto}[0]
+        puts "dppuerto=#{dppuerto}"
+        if dppuerto[:ppid] != dmiproc[:ppid]
+          Process.kill("KILL", ppuerto)
+        else
+          # Verificamos que la aplicación que corre en el puerto reservado
+          # esté respondiedo los esperado
+          res = ''
+          open("http://#{@fil23_gen_op[:ip]}:#{@fil23_gen_op[:puerto]}") {|f|
+                f.each_line {|line| res << line + "\n"}
+          } 
+          if !res.include?('shiny')
+            Process.kill("KILL", ppuerto)
+            ini = true
+          else
+            ini = false
+          end
+        end
+      end
+      if ini
+      #if Fil23Gen::Bifurcacion.all.count > 0
+      #  ult_numproc = Fil23Gen::Bifurcacion.order(:marcatemporal).last.numproc
+      #  pact = lproc.select{|p| p[:pid].to_i == ult_numproc} #&&
+      #                      #/\/R /.match(p[:command]) }
+      #end
+      #if pact.count == 0
+        begin
+          ::R.quit
+        rescue
+          puts "::R.quit no funcionó (tal vez ya se había ejecutado)"
+        end
+        numproc = fork {
+          # No usamo aqui ::R porque notamos que puede usar un proceso
+          # de R pegado al proceso principal de ruby asi que bloquea
+          # la aplicación completa
+          miR = RinRuby.new
+          miR.eval <<-EOF
+            print("Iniciando aplicacion R")
+            write(getwd(), stdout())
+            rutacsv="#{@fil23_gen_op[:rutacsv]}"
+            source("#{@fil23_gen_op[:guionR]}")
+          EOF
+          #      byebug
+          miR.eval <<-EOF
+            shinyApp(ui = interfaz, server = servidor, 
+            options = list(host = "#{@fil23_gen_op[:ip]}", 
+            port = #{@fil23_gen_op[:puerto]}))
+          EOF
+          return false # hijo no necesita hacer render
+        }
+        if numproc > 0 
+          # Fil23Gen::Bifurcacion.create(numproc: numproc,
+          #   marcatemporal: Time.now)
+          sleep 2 # Damos tiempo a aplicacion shiny de arranca antes de continuar presentando iframe
+        else
+          puts "No pudo bifurcarse proceso"
+          render inline: 'No pudo bifurcarse proceso para shiny', 
+            layout: 'application'
+          return false
+        end
+      else
+        puts "Ya existe proceso #{dppuerto[:pid]} con estado #{dppuerto[0][:stat]}"
+      end
+
+      return true
+    end
+
     def victimizaciones_individuales
       authorize! :contar, Sivel2Gen::Caso
 
@@ -20,17 +177,8 @@ module Fil23Gen
         "supracategoria.id_tviolencia || categoria.id || ' ' || categoria.nombre AS categoria_rotulo, " \
         "acto.id_persona AS persona_id, " \
         "persona.sexo AS sexonac, " \
-        "victima.id_rangoedad AS rangoedad_id, " \
-        "rangoedad.nombre AS rangoedad_nombre, " \
-        "victima.id_filiacion AS filiacion_id, " \
-        "filiacion.nombre AS filiacion_nombre, " \
-        "victima.id_organizacion AS organizacion_id, " \
-        "organizacion.nombre AS organizacion_nombre, " \
-        "victima.id_profesion AS profesion_id, " \
-        "profesion.nombre AS profesion_nombre, " \
-        "victima.id_sectorsocial AS sectorsocial_id, " \
-        "sectorsocial.nombre AS sectorsocial_nombre, " \
         "ubicacion.id_departamento AS departamento_id, " \
+        "departamento.nombre AS departamento, " \
         "ubicacion.id_municipio AS municipio_id " \
         "FROM  sivel2_gen_acto AS acto " \
         "JOIN sivel2_gen_caso AS caso ON caso.id = acto.id_caso " \
@@ -40,13 +188,9 @@ module Fil23Gen
         "JOIN sivel2_gen_victima AS victima ON " \
         "  victima.id_caso=acto.id_caso AND " \
         "  victima.id_persona=acto.id_persona " \
-        "JOIN sivel2_gen_rangoedad AS rangoedad ON rangoedad.id = victima.id_rangoedad " \
-        "JOIN sivel2_gen_filiacion AS filiacion ON filiacion.id = victima.id_filiacion " \
-        "JOIN sivel2_gen_organizacion AS organizacion ON organizacion.id = victima.id_organizacion " \
-        "JOIN sivel2_gen_profesion AS profesion ON profesion.id = victima.id_profesion " \
-        "JOIN sivel2_gen_sectorsocial AS sectorsocial ON sectorsocial.id = victima.id_sectorsocial " \
-        "LEFT JOIN sip_ubicacion AS ubicacion ON ubicacion.id=caso.ubicacion_id)" \
-        " TO '#{rutatmp}' DELIMITER ',' CSV HEADER;" 
+        "LEFT JOIN sip_ubicacion AS ubicacion ON ubicacion.id=caso.ubicacion_id " \
+        "LEFT JOIN sip_departamento AS departamento ON ubicacion.id_departamento=departamento.id " \
+        ") TO '#{rutatmp}' DELIMITER ',' CSV HEADER;" 
       res = ActiveRecord::Base.connection.execute(sql)
       if File.exist?(@rutacsv)
         File.unlink(@rutacsv)
@@ -62,10 +206,13 @@ module Fil23Gen
         ip: ENV['fil23_gen_ip'],
         puerto: ENV['fil23_gen_puerto'],
         protocolo: ENV['fil23_gen_protocolo'],
-        guionR: 'lib/R/victimizaciones_por_sexo.R',
+        guionR: 'lib/R/victimizaciones_individuales.R',
         rutacsv: @rutacsv
       }
-      render 'fil23_gen/graficar_r/graficar', layout: 'application'
+
+      if arranca_ap_shiny #@fil23_gen_op
+        render 'fil23_gen/graficar_r/graficar', layout: 'application'
+      end
     end
 
   end
---
+++ lib/R/victimizaciones_individuales.R
@@ -0,0 +1,259 @@
+# Victimizaciones por sexo de nacimiento
+# Autor inicial: Andres Mauricio Galeano agaleano@cinep.org.co
+
+# Nomenclatura: shiny usa CamelCase comenzando en minúscula
+# R usa más _
+# Usamos más _
+# Variables que podamos poner en español, las ponemos en español
+
+print("** victimizaciones por sexo")
+if(!require(shiny)){
+  install.packages("shiny", repos = "https://www.icesi.edu.co/CRAN/")
+}
+library(shiny)
+if(!require(ggplot2)){
+  install.packages("ggplot2", repos = "https://www.icesi.edu.co/CRAN/")
+}
+library(ggplot2)
+if(!require(rio)){
+  install.packages("rio", repos = "https://www.icesi.edu.co/CRAN/")
+}
+library(rio)
+if(!require(plyr)){
+  install.packages("plyr", repos = "https://www.icesi.edu.co/CRAN/")
+}
+library(plyr)
+if(!require(dplyr)){
+  install.packages("dplyr", repos = "https://www.icesi.edu.co/CRAN/")
+}
+library(dplyr)
+if(!require(svglite)){
+  install.packages("svglite", repos = "https://www.icesi.edu.co/CRAN/")
+}
+if(!require(shinydashboard)){
+  install.packages("shinydashboard", repos = "https://www.icesi.edu.co/CRAN/")
+  library(shinydashboard)
+}
+library(svglite)
+print("** dependencias completas")
+
+
+# Preparación de datos
+print(rutacsv)
+tabla <- import(rutacsv)
+tabla$sexonac <- factor(tabla$sexonac)
+tabla$fecha <- as.Date(tabla$fecha)
+tabla$categoria_rotulo <- factor(tabla$categoria_rotulo)
+tabla$departamento <- factor(tabla$departamento)
+tablasub <- subset(tabla, select  = 
+  c(fecha, sexonac, departamento, categoria_rotulo))
+tablasub <- dplyr::rename(tablasub, SexoNac=sexonac, Fecha=fecha,
+  Departamento=departamento,Categoria=categoria_rotulo)
+
+minfecha = min(tablasub$Fecha)
+print(minfecha)
+maxfecha = max(tablasub$Fecha)
+print(maxfecha)
+
+print("Por generar interfaz de usuario")
+# Interfaz de usuario
+interfaz <- dashboardPage(skin = "blue",
+  dashboardHeader(disable = T),
+  dashboardSidebar(
+    dateRangeInput(inputId = "rango",
+      label = "Rango de fechas de la victimización",
+      start = minfecha,
+      end = maxfecha,
+      format = "yyyy-mm-dd"),
+    varSelectInput("var", "Seleccione variable",
+      data = tablasub[,2:4],
+      multiple = F,
+      selected = NULL,
+      selectize = T),
+    selectInput("sexonac", "Sexos incluidos",
+      choices = levels(tablasub$SexoNac),
+      multiple = T,
+      selected = levels(tablasub$SexoNac)),
+    selectInput("categoria_rotulo", "Categorias de violencia incluidas",
+      choices = levels(tablasub$Categoria),
+      multiple = T,
+      selected = levels(tablasub$Categoria)),
+    selectInput("departamento", "Departamentos incluidos",
+      choices = levels(tablasub$Departamento),
+      multiple = T,
+      selected = levels(tablasub$Departamento))
+    ),
+  dashboardBody(
+    tags$head(tags$style(HTML('.info-box {min-height: 45px; width: 300px;} .info-box-icon {height: 45px; line-height: 45px;} .info-box-content {padding-top: 0px; padding-bottom: 0px;}'))),
+    fluidRow(infoBoxOutput("vict_n")),
+    fluidRow(
+      box(
+        title = "Serie de tiempo", 
+        status = "primary", 
+        solidHeader = T,
+        collapsible = T,
+        plotOutput("graficar_serie"),
+        downloadButton("descargar_serie_SVG", "SVG"),
+        downloadButton("descargar_serie_PNG", "PNG"),
+        downloadButton("descargar_serie_CSV", "CSV")
+        ),
+      box(
+        title = "Totales",
+        status = "primary",
+        solidHeader = T,
+        collapsible = T,
+        plotOutput("graficar_total"),
+        downloadButton("descargar_total_SVG", "SVG"),
+        downloadButton("descargar_total_PNG", "PNG"),
+        downloadButton("descargar_total_CSV", "CSV")
+      )
+    )
+  )
+)
+
+print("Por definir servidor")
+# Lógica para dibujar en la interfaz
+servidor <- function(input, output, session) {
+
+  datos <- reactive({
+    subset(tablasub, SexoNac %in% input$sexonac &
+        Categoria %in% input$categoria_rotulo &
+        Departamento %in% input$departamento &
+        Fecha >= input$rango[1] & Fecha <= input$rango[2])
+  })
+
+  serie <- reactive({
+    plyr::count(datos(), c('Fecha', 'SexoNac', 'Departamento', 'Categoria'))
+  })
+
+  var_input <- reactive({ input$var })
+
+  total <- reactive({
+    plyr::count(serie(), c('SexoNac', 'Departamento', 'Categoria'))
+  })
+
+  graficar_serie <- reactive({
+    if (input$var == 'SexoNac') {
+      print("SexoNac")
+      ggplot(data = serie(), aes(x = Fecha, y = freq, group = SexoNac, 
+          colour=SexoNac)) +
+      geom_line() + geom_point() +
+      labs(title='Serie de tiempo por sexo de nacimiento',
+        y='Victimizaciones', x= 'Fecha') +
+      theme(legend.position="top", legend.title = element_blank())
+    } else if (input$var == 'Departamento') {
+      print("Departamento")
+      ggplot(data = serie(), aes(x = Fecha, y = freq, group = Departamento, 
+          colour=Departamento)) +
+      geom_line() + geom_point() +
+      labs(title='Serie de tiempo por departamento',
+        y='Victimizaciones', x= 'Fecha') +
+      theme(legend.position="top", legend.title = element_blank())
+    } else if (input$var == 'Categoria') {
+      print("Categoria")
+      ggplot(data = serie(), aes(x = Fecha, y = freq, group = Categoria, 
+          colour=Categoria)) +
+      geom_line() + geom_point() +
+      labs(title='Serie de tiempo por categoria',
+        y='Victimizaciones', x= 'Fecha') +
+      theme(legend.position="top", legend.title = element_blank())
+    }
+
+  })
+
+  graficar_total <- reactive({
+    if (input$var == 'SexoNac') {
+      ggplot(data = datos(), aes(x = SexoNac, fill = SexoNac))+
+        geom_bar()+ ylab("Victimizaciones") +
+        geom_text(stat = 'count', aes(label = ..count..), vjust = 1)
+        #geom_bar(stat="identity", position=position_dodge()) +
+        #geom_text(aes(label=freq), vjust=1.6, color="white",
+        #  position = position_dodge(0.9), size=3.5) +
+        #labs(title='Totales por sexo de nacimiento',
+        #  y='Victimizaciones', x= 'Sexo de nacimiento') +
+        #theme(legend.position="none")
+    } else if (input$var == 'Departamento') {
+      ggplot(data = datos(), aes(x = Departamento, fill = Departamento))+
+        geom_bar()+ ylab("Victimizaciones") +
+        geom_text(stat = 'count', aes(label = ..count..), vjust = 1)
+      #ggplot(data = total(), aes(x = Departamento , y = freq, fill = Departamento))+
+      #  geom_bar(stat="identity", position=position_dodge()) +
+      #  geom_text(aes(label=freq), vjust=1.6, color="white",
+      #    position = position_dodge(0.9), size=3.5) +
+      #  labs(title='Totales por departamento',
+      #    y='Victimizaciones', x= 'Departamento') +
+      #  theme(legend.position="none")
+    } else if (input$var == 'Categoria') {
+      ggplot(data = datos(), aes(x = Categoria, fill = Categoria))+
+        geom_bar()+ ylab("Victimizaciones") +
+        geom_text(stat = 'count', aes(label = ..count..), vjust = 1)
+      #ggplot(data = total(), aes(x = Categoria, y = freq, fill = Categoria))+
+      #  geom_bar(stat="identity", position=position_dodge()) +
+      #  geom_text(aes(label=freq), vjust=1.6, color="white",
+      #    position = position_dodge(0.9), size=3.5) +
+      #  labs(title='Totales por categoria',
+      #    y='Victimizaciones', x= 'Categoria') +
+      #  theme(legend.position="none")
+    } 
+
+  })
+
+  output$graficar_serie <- renderPlot({
+    graficar_serie()
+  })
+
+  output$graficar_total <- renderPlot({
+    graficar_total()
+  })
+
+
+  output$descargar_serie_SVG<- downloadHandler(
+    filename = function() { paste("serie-sexonac", ".svg", sep = "") },
+    content = function(nomarc){
+      ggsave(nomarc, graficar_serie())
+    }
+  )
+
+  output$descargar_serie_PNG<- downloadHandler(
+    filename = function() {paste("serie-sexonac", ".png", sep = "")},
+    content = function(nomarc){
+      ggsave(nomarc, graficar_serie())
+    }
+  )
+
+  output$descargar_serie_CSV<- downloadHandler(
+    filename = function() {paste("serie-sexonac", ".csv", sep = "")},
+    content = function(nomarc){
+      print(nomarc)
+      write.csv(serie(), nomarc)
+    }
+  )
+
+  output$descargar_total_SVG <- downloadHandler(
+    filename = function() {paste("total-sexonac", ".svg", sep = "")},
+    content = function(nomarc){
+      ggsave(nomarc, graficar_total())
+    }
+  )
+
+  output$descargar_total_PNG <- downloadHandler(
+    filename = function() {paste("total-sexonac", ".png", sep = "")},
+    content = function(nomarc){
+      ggsave(nomarc,graficar_total())
+    }
+  )
+
+  output$descargar_total_CSV <- downloadHandler(
+    filename = function() {paste("total-sexonac", ".csv", sep = "")},
+    content = function(nomarc){
+      print(nomarc)
+      write.csv(total(), nomarc)
+    }
+  )
+}
+
+print("Por ejecutar shinyApp")
+# La aplicación se ejecutaría así:
+#shinyApp(ui = interfaz, server = servidor, 
+#  options = list(host = "181.143.184.115", port = 2902))
+
