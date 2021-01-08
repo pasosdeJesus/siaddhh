bbf377b78b2cddf680c1e785efad673da3ae3a3b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-01-08 08:00:10 -0500

    Devuelve a rails 6.0 con suavidad

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '>=2.4'
+ruby '>=2.7.1'
 
 
 gem 'bcrypt'
@@ -25,7 +25,7 @@ gem 'libxml-ruby'
 
 gem 'odf-report' # Genera ODT
 
-gem 'nokogiri', '>=1.11.0.rc4'
+gem 'nokogiri', '>=1.11.1'
 
 gem 'paperclip' # Maneja adjuntos
 
@@ -39,7 +39,7 @@ gem 'prawnto_2',  :require => 'prawnto'
 
 gem 'prawn-table'
 
-gem 'rails', '~> 6.1.0'
+gem 'rails', '~> 6.0.3.4'
 
 gem 'rails-i18n'
 
--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,10 @@ Bundler.require(*Rails.groups)
 module Sivel2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
+    # Application configuration can go into files in config/initialize
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
