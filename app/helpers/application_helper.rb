30769683603f8e39946bf276061b397d807bccac
Nombre Usuario <correo@usuario.net>
2021-10-04 06:30:52 -0500

    mapa de calor en blanco

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
   include Sivel2Gen::ApplicationHelper 
+
+  # Basado en función de https://thoughtbot.com/blog/organized-workflow-for-svg
+  def embedded_svg(filename, options = {})
+    assets = Rails.application.assets
+    file = assets.find_asset(filename).source.force_encoding("UTF-8")
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css "svg"
+    if options[:class].present?
+      svg["class"] = options[:class]
+    end
+    raw doc
+  end
 end
