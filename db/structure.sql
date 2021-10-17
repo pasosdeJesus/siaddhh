0a82a3531a1175b2852ec540108154d7c2dcf050
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-10-17 06:11:13 -0500

    Actualiza ruby y node en gitlab ci

--- db/structure.sql
+++ db/structure.sql
@@ -660,8 +660,7 @@ CREATE VIEW public.cvt1 AS
      JOIN public.sivel2_gen_victima victima ON (((victima.id_persona = acto.id_persona) AND (victima.id_caso = caso.id))))
      JOIN public.sip_persona persona ON ((persona.id = acto.id_persona)))
      LEFT JOIN public.sip_ubicacion ubicacion ON ((caso.ubicacion_id = ubicacion.id)))
-     LEFT JOIN public.sip_departamento departamento ON ((ubicacion.id_departamento = departamento.id)))
-  WHERE (caso.fecha >= '2021-10-13'::date);
+     LEFT JOIN public.sip_departamento departamento ON ((ubicacion.id_departamento = departamento.id)));
 
 
 --
