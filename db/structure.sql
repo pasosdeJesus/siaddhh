b26f5447606fbec04e32ad248e65a2cb7f4fdde4
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-04-13 17:10:33 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -1631,7 +1631,7 @@ CREATE TABLE public.sip_orgsocial_sectororgsocial (
 CREATE TABLE public.sip_pais (
     id integer NOT NULL,
     nombre character varying(200) COLLATE public.es_co_utf_8,
-    nombreiso character varying(200),
+    nombreosm character varying(200),
     latitud double precision,
     longitud double precision,
     alfa2 character varying(2),
@@ -1644,7 +1644,9 @@ CREATE TABLE public.sip_pais (
     fechadeshabilitacion date,
     created_at timestamp without time zone,
     updated_at timestamp without time zone,
-    observaciones character varying(5000) COLLATE public.es_co_utf_8
+    observaciones character varying(5000) COLLATE public.es_co_utf_8,
+    nombreiso_ingles character varying(512),
+    nombreiso_frances character varying(512)
 );
 
 
@@ -6474,6 +6476,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20220316025851'),
 ('20220323001338'),
 ('20220323001645'),
-('20220323004929');
+('20220323004929'),
+('20220413123127');
 
 
