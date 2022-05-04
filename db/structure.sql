9d7eee4a25af39b698551439914d429eef84caa3
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-05-04 08:29:25 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -1558,7 +1558,8 @@ CREATE TABLE public.sip_orgsocial (
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
     fechafundacion date,
-    fechadeshabilitacion date
+    fechadeshabilitacion date,
+    tipoorg_id integer DEFAULT 2 NOT NULL
 );
 
 
@@ -1942,6 +1943,40 @@ CREATE SEQUENCE public.sip_tipoanexo_id_seq
 --
 
 ALTER SEQUENCE public.sip_tipoanexo_id_seq OWNED BY public.sip_tipoanexo.id;
+
+
+--
+-- Name: sip_tipoorg; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.sip_tipoorg (
+    id bigint NOT NULL,
+    nombre character varying(500) NOT NULL COLLATE public.es_co_utf_8,
+    observaciones character varying(5000),
+    fechacreacion date NOT NULL,
+    fechadeshabilitacion date,
+    created_at timestamp(6) without time zone NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL
+);
+
+
+--
+-- Name: sip_tipoorg_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.sip_tipoorg_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: sip_tipoorg_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.sip_tipoorg_id_seq OWNED BY public.sip_tipoorg.id;
 
 
 --
@@ -3548,6 +3583,13 @@ ALTER TABLE ONLY public.sip_tipoanexo ALTER COLUMN id SET DEFAULT nextval('publi
 
 
 --
+-- Name: sip_tipoorg id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sip_tipoorg ALTER COLUMN id SET DEFAULT nextval('public.sip_tipoorg_id_seq'::regclass);
+
+
+--
 -- Name: sip_trivalente id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -4203,6 +4245,14 @@ ALTER TABLE ONLY public.sip_tema
 
 ALTER TABLE ONLY public.sip_tipoanexo
     ADD CONSTRAINT sip_tipoanexo_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: sip_tipoorg sip_tipoorg_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sip_tipoorg
+    ADD CONSTRAINT sip_tipoorg_pkey PRIMARY KEY (id);
 
 
 --
@@ -6512,6 +6562,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20211001090946'),
 ('20211001092004'),
 ('20211001101742'),
+('20211010164634'),
 ('20211011214752'),
 ('20211011233005'),
 ('20211019121200'),
@@ -6537,6 +6588,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20220417221010'),
 ('20220420143020'),
 ('20220420154535'),
-('20220422190546');
+('20220422190546'),
+('20220428145059');
 
 
